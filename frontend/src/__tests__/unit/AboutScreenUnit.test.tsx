ca08b7d66c5ecf504b1a5f85eb3b9d54f89219fc
SrVinicius <viniciusferreirarosario5@gmail.com>
2025-07-22 17:35:33 -0300

    generic test

--- frontend/src/__tests__/unit/AboutScreenUnit.test.tsx
+++ frontend/src/__tests__/unit/AboutScreenUnit.test.tsx
@@ -25,3 +25,11 @@
 //     expect(acessarButton).toBeInTheDocument();
 //   });
 // });
+
+import { describe, it, expect } from 'vitest';
+
+describe('Operações matemáticas simples', () => {
+  it('verifica que 1 + 1 = 2', () => {
+    expect(1 + 1).toBe(2);
+  });
+});
